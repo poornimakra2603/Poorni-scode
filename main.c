1b41aef422d0d767fa4a7d3e0c243998ecd721ea
poornimakra2603 <112956034+poornimakra2603@users.noreply.github.com>
2022-09-15 18:41:18 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1;
+    int num2;
+    printf("Enter a firstnumber:");
+    scanf("%d",&num1);
+    printf("Enter a secondnumber:");
+    scanf("%d",&num2);
+    printf("Output:%d",num1+num2);
+    return 0;
+}
